f41ff78c950685b4db66fb152b808ddbb0382c84
360nobuggs <vascoduel@gmail.com>
2020-12-04 17:45:14 +0000

    Update ListaAdapter.kt

--- app/src/main/java/pt/isec/a2017014841/tp/other/ListaAdapter.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/other/ListaAdapter.kt
@@ -4,27 +4,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.show_listas.view.*
 import pt.isec.a2017014841.tp.R
 import pt.isec.a2017014841.tp.data.db.entidades.Lista
 
 
-class ListaAdapter(
-    var listas: List<Lista>
-): RecyclerView.Adapter<ListaAdapter.ListaViewHolder>() {
-    inner class ListaViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  //representa cada um dos itens que vai ser visualizado dentro da RV
+class ListaAdapter(private var listas: List<Lista>): RecyclerView.Adapter<ListaAdapter.ListaViewHolder>() {
+    inner class ListaViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        val nameTextView = itemView.findViewById<TextView>(R.id.nome_lista)//texto da lista
+    }//representa cada um dos itens que vai ser visualizado dentro da RV
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaViewHolder {    //
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.show_listas,parent,false)
-        return ListaViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaAdapter.ListaViewHolder {    //
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val view_lista = inflater.inflate(R.layout.show_listas, parent, false)
+
+
+
+        return ListaViewHolder(view_lista)
     }
 
     override fun getItemCount(): Int {
         return listas.size
     }
 
-    override fun onBindViewHolder(holder: ListaViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ListaAdapter.ListaViewHolder, position: Int) {
+        /*val nome:
+
+
+        val contact: Contact = mContacts.get(position)
+        // Set item views based on your views and data model
+        val textView = viewHolder.nameTextView
+        textView.setText(contact.name)
+        val button = viewHolder.messageButton
+        button.text = if (contact.isOnline) "Message" else "Offline"
+        button.isEnabled = contact.isOnline*/
+
         holder.itemView.apply{
             rvtextView.text = listas[position].nome
         }
