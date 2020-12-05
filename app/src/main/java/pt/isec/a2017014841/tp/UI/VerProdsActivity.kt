e4eb190a6fc1e6590524845f1e4fb4bc56cb7b62
360nobuggs <vascoduel@gmail.com>
2020-12-05 17:05:25 +0000

    Update VerProdsActivity.kt

--- app/src/main/java/pt/isec/a2017014841/tp/UI/VerProdsActivity.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/UI/VerProdsActivity.kt
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import pt.isec.a2017014841.tp.R
+import pt.isec.a2017014841.tp.other.ItemAdapter
+import pt.isec.a2017014841.tp.other.ListaAdapter
 
 class VerProdsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_prods)
-
         this.setTitle("Produtos")
+        val rvlista = findViewById<View>(R.id.rvItems) as RecyclerView
+       // val adapter = ItemAdapter(listas)
+       // rvlista.adapter = adapter
+        rvlista.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
