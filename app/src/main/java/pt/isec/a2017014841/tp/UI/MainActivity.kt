ad4ccabce33051e90dd042158283b8012101def2
360nobuggs <vascoduel@gmail.com>
2020-12-05 00:06:46 +0000

    Update MainActivity.kt

--- app/src/main/java/pt/isec/a2017014841/tp/UI/MainActivity.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/UI/MainActivity.kt
@@ -2,47 +2,53 @@ package pt.isec.a2017014841.tp.UI
 
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
-import android.widget.ListView
+import android.widget.ListAdapter
 import androidx.appcompat.app.AlertDialog
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import pt.isec.a2017014841.tp.R
-import pt.isec.a2017014841.tp.UI.produtos.ProdViewModel
 import pt.isec.a2017014841.tp.data.db.DB
 import pt.isec.a2017014841.tp.data.repositorios.ProdRepositorio
-import java.lang.NullPointerException
+import pt.isec.a2017014841.tp.other.ListaAdapter
 
 class MainActivity : AppCompatActivity() {
-    private var database = DB(this)
-    private var repositorio = ProdRepositorio(database)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var database = DB(this)
+        var repositorio = ProdRepositorio(database)
         this.setTitle("ListMaker")
-        val rvlista = findViewById<View>(R.id.rvListas) as RecyclerView
-       // contacts = Contact.createContactsList(20)
-        // val adapter = ContactsAdapter(contacts)
-        //rvlista.adapter = adapter
-        rvlista.layoutManager = LinearLayoutManager(this)
-        //val factory
-        //val viewModel = ViewModelProvider(this, factory).get(ProdViewModel())
 
-        rvListas.layoutManager=LinearLayoutManager(this)
+       // contacts = Contact.createContactsList(20)
+
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        val cars = ArrayList<String>()
+        cars.add("um")
+        cars.add("dois")
+        cars.add("tres")
+        cars.add("quatro")
+        val rvlista = findViewById<View>(R.id.rvListas) as RecyclerView
+        //val factory
+        //val viewModel = ViewModelProvider(this, factory).get(ProdViewModel())
+        val adapter = ListaAdapter(cars)
+        rvlista.adapter = adapter
+        rvlista.layoutManager = LinearLayoutManager(this)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu1,menu)
+        menuInflater.inflate(R.menu.menu1, menu)
         return true
     }
 
@@ -56,15 +62,17 @@ class MainActivity : AppCompatActivity() {
             with(builder) {
                 setTitle("Indique o nome da lista")
                 setPositiveButton("OK") { dialogInterface: DialogInterface, i: Int ->
-                    val intent = Intent(this.context,
-                        VerListaActivity::class.java).also{
+                    val intent = Intent(
+                        this.context,
+                        VerListaActivity::class.java
+                    ).also{
                         val nome = editText.text.toString()
                         it.putExtra("NOME_LISTA", nome)
                         startActivity(it)
                     }
 
                 }
-                setNegativeButton("Cancelar"){dialog, which->
+                setNegativeButton("Cancelar"){ dialog, which->
                     Log.d("Main", "Negative button clicked")
                 }
                 setView(dialogLayout)
