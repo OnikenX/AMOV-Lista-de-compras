c447567f0e6a57a7c614d9fbb8b2d55bb010c7f8
OnikenX <OnikenX@protonmail.com>
2020-12-05 13:28:13 +0000

    uma tentativa de merda

--- app/src/main/java/pt/isec/a2017014841/tp/UI/NovoProdActivity.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/UI/NovoProdActivity.kt
@@ -1,25 +1,31 @@
 package pt.isec.a2017014841.tp.UI
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_novo_prod.*
 import pt.isec.a2017014841.tp.R
 import java.io.File
+import java.io.IOException
 
 
 class NovoProdActivity : AppCompatActivity() {
+
+    val RESULT_LOAD_IMAGE = 1
+    val
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_prod)
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.setType("image/*")
-        startActivityForResult(intent, 10)
+        //val intent = Intent(Intent.ACTION_PICK)
+        //intent.setType("image/*")
+        //startActivityForResult(intent, 10)
         //id -> id do pedido startActivityForREsult
 
         add_photo.setOnClickListener {
@@ -36,23 +42,32 @@ class NovoProdActivity : AppCompatActivity() {
             val dialogBuilder = AlertDialog.Builder(this)
                 .setTitle(getString(R.string.how_to_photo))
                 .setItems(items) { dialog, item ->
-                    run {
-                        if (items[item] == getString(R.string.take_photo)) {
-                             intentchoose = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                            startActivityForResult(intent, REQUEST_CAMERA)
-                        } else if (items[item] == getString(R.string.choose_from_gallery)) {
-                             intentchoose = Intent(
-                                Intent.ACTION_PICK,
-                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-
-                            )
-                            startActivityForResult(intent, SELECT_FILE)
-                        } else if (items[item] == getString(R.string.cancel)) {
-                            dialog.dismiss()
+                    run{
+                        Log.wtf("WTF", "IS GOING ON")
+                        when {
+                            items[item] == getString(R.string.take_photo) -> {
+                                intentchoose = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                                startActivityForResult(intent, REQUEST_CAMERA)
+                            }
+                            items[item] == getString(R.string.choose_from_gallery) -> {
+                                try {
+                                    val i = Intent(
+                                        Intent.ACTION_PICK,
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                                    )
+                                    startActivityForResult(i, RESULT_LOAD_IMAGE)
+                                } catch (exp: Exception) {
+                                    Log.i("Error", exp.toString())
+                                }
+                            }
+                            items[item] == getString(R.string.cancel) -> {
+                                dialog.dismiss()
+                            }
                         }
                     }
                 }
                 .show()
+
         }
 
 
@@ -67,4 +82,38 @@ class NovoProdActivity : AppCompatActivity() {
         }
         startActivityForResult(intent, 20)
     }
+
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            val selectedImage = data.data
+            val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor: Cursor? = getContentResolver().query(
+                selectedImage!!,
+                filePathColumn, null, null, null
+            )
+            cursor.moveToFirst()
+            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+            val picturePath: String = cursor.getString(columnIndex)
+            cursor.close()
+            try {
+                bmp = getBitmapFromUri(selectedImage)
+            } catch (e: IOException) {
+                // TODO Auto-generated catch block
+                e.printStackTrace()
+            }
+            image_view.setImageBitmap(bmp)
+
+            //to know about the selected image width and height
+            Toast.makeText(
+                this@NovoProdActivity,
+                image_view.getDrawable().getIntrinsicWidth()
+                    .toString() + " & " + image_view.getDrawable().getIntrinsicHeight(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 }
+
