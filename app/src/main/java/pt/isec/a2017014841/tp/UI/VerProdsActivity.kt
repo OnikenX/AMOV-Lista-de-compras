feeb5a1d0b3a35690ed0fcb549cebb9a4d014da5
360nobuggs <vascoduel@gmail.com>
2020-12-05 18:48:04 +0000

    Update VerProdsActivity.kt

--- app/src/main/java/pt/isec/a2017014841/tp/UI/VerProdsActivity.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/UI/VerProdsActivity.kt
@@ -1,30 +1,39 @@
 package pt.isec.a2017014841.tp.UI
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import pt.isec.a2017014841.tp.R
+import pt.isec.a2017014841.tp.data.classes.Item
 import pt.isec.a2017014841.tp.other.ItemAdapter
-import pt.isec.a2017014841.tp.other.ListaAdapter
 
 class VerProdsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("DEBUG", "MyClass.getView() — get item number")
         setContentView(R.layout.activity_ver_prods)
         this.setTitle("Produtos")
+        val intent = intent
+        var lista_items:ArrayList<Item>
+        val bundle_main = intent.extras
+        if (bundle_main!=null)
+        {
+            lista_items = bundle_main.getSerializable("ARRAYLIST") as ArrayList<Item>
+        }
         val rvlista = findViewById<View>(R.id.rvItems) as RecyclerView
-       // val adapter = ItemAdapter(listas)
-       // rvlista.adapter = adapter
+        //val adapter = ItemAdapter(lista_items)
+        //rvlista.adapter = adapter
         rvlista.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu3,menu)
+        menuInflater.inflate(R.menu.menu3, menu)
         return true
     }
 
