b78f82e104d2d9f10c050f9212db6354fbe7be19
360nobuggs <vascoduel@gmail.com>
2020-12-06 02:27:02 +0000

    dddd

--- app/src/main/java/pt/isec/a2017014841/tp/data/classes/Lista_items.kt
+++ app/src/main/java/pt/isec/a2017014841/tp/data/classes/Lista_items.kt
@@ -3,6 +3,10 @@ package pt.isec.a2017014841.tp.data.classes
 import java.io.Serializable
 
 class Lista_items(private var nome: String, private var lista: ArrayList<Item>) : Serializable{
-    fun get_nome(): String {return nome;}
+    fun get_nome(): String {return nome}
     fun get_items(): ArrayList<Item> {return lista}
+    fun set_items(o :ArrayList<Item>)
+    {
+        lista=o;
+    }
 }
